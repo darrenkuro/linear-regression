0293eb6109502217911ec27dd478bd0e8f831d40
darrenkuro <odon5ht@gmail.com>
2025-12-09 22:04:32 +0100

    Move source files to src dir

--- src/predict.py
+++ src/predict.py
@@ -2,31 +2,29 @@
 
 import json
 import os
+import sys
 
 def load_model(path="model.json"):
     if not os.path.exists(path):
-		print("Cannot find file. Please check that path is correct.")
+        print("Cannot find model file.", file=sys.stderr)
         exit(1)
-    with open(path) as f:
+    with open(path) as f: # what happens if no read access
         data = json.load(f)
-	if !data["trained"]:
-		print("Model is untrained. Please run train.py first.")
-		exit(1)
+    if not data["trained"]:
+        print("Model is untrained. Please run train.py first.", file=sys.stderr)
+        exit(1)
     return data["theta0"], data["theta1"]
 
 def main():
     theta0, theta1 = load_model()
-
     try:
         mileage = float(input("Enter mileage (in km): "))
     except ValueError:
         print("Invalid input!")
         return
-
     price = theta0 + theta1 * mileage
     if price < 0:
         price = 0
-
     print(f"Estimated price: {price:.2f}")
 
 if __name__ == "__main__":
